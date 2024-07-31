57ce52f0587831f993849dade94717231a25d1a8
Ashley Watson-Nolan <nolly00@gmail.com>
2024-07-31 10:39:23 +0100

    First commit of weather app – copy from replit

---
+++ src/components/BaseModal.vue
@@ -0,0 +1,41 @@
+<template>
+  <Teleport to="body">
+    <Transition name="modal-outer">
+      <div
+        v-show="modalActive"
+        class="absolute w-full bg-black bg-opacity-30 h-screen top-0 left-0 flex justify-center px-8"
+      >
+        <Transition name="modal-inner">
+          <div
+            v-if="modalActive"
+            class="p-4 bg-white self-start mt-32 max-w-screen-md"
+          >
+            <slot />
+            <button
+              class="text-white mt-8 bg-weather-primary py-2 px-6"
+              @click="$emit('close-modal')"
+            >
+              Close
+            </button>
+          </div>
+        </Transition>
+      </div>
+    </Transition>
+  </Teleport>
+</template>
+
+<script setup>
+// Emit custom event so we can close the modal when the event is triggered.
+defineEmits(["close-modal"]);
+
+// Define props to we can control the visibility of the modal.
+defineProps({
+  modalActive: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+</script>
+
+<style lang="scss" scoped></style>
